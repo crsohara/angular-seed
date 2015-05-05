f8ff5a079604380c05ffc9531a9c73697052cbf5
chris ohara <crsohara@gmail.com>
2015-05-05 23:21:41 +0300

    create currency converter

--- app/app.js
+++ app/app.js
@@ -1,12 +1,84 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+var exchangeApp = angular.module('exchangeApp', [
+    'ngRoute',
+    'exchangeController',
+]);
+
+var exchangeController = angular.module('exchangeController', []);
+
+
+exchangeController.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/view1', {
+        templateUrl: 'partials/view1.html',
+      });
 }]);
+
+exchangeController.constant('FixerServiceUrl',{
+        url: "http://api.fixer.io/latest?",
+        controller: 'exchangeController'
+    });
+
+exchangeController.controller('exchangeController', ['$scope', 'fixerServiceBase', 'fixerService', 'FixerServiceUrl',
+    function($scope, fixerServiceBase, fixerService, FixerServiceUrl){
+
+        fixerServiceBase.getCurrencies()
+            .then(function(data) {
+                var keys = [];
+
+                keys.push(data.base);
+
+                angular.forEach(data.rates, function(value, key) {
+                    keys.push(key);
+                });
+
+                $scope.currencies = keys;
+                $scope.baseCurr = 'EUR';
+                $scope.convCurr = 'USD';
+            });
+
+        $scope.go = function() {
+            fixerService.getConversion($scope.baseCurr, $scope.convCurr)
+                    .then(function(data) {
+                        $scope.fromToRate = data.rates[$scope.convCurr];
+                    });
+
+                fixerService.getConversion($scope.convCurr, $scope.baseCurr)
+                    .then(function(data) {
+                        // $scope.currencies = data.rates;
+                        $scope.toFromRate = data.rates[$scope.baseCurr];
+                    });
+        };
+    }]);
+
+exchangeController.factory("fixerServiceBase", ['$http', 'FixerServiceUrl', 
+    function($http, FixerServiceUrl) {
+        var getCurrencies = function(){
+            return $http.get(FixerServiceUrl.url)
+                .then(function(response){
+                    return response.data;
+                });
+            };
+        return {
+            getCurrencies: getCurrencies
+        };
+
+    }]);
+
+exchangeController.factory("fixerService", ['$http', 'FixerServiceUrl', 
+    function($http, FixerServiceUrl) {
+
+        var getConversion = function(BaseCurrency, ConversionCurrency){
+            return $http.get(FixerServiceUrl.url+"base="+BaseCurrency+"&symbols="+ConversionCurrency)
+                .then(function(response){
+                        return response.data;
+                });
+            };
+
+        // Exposing functions
+        return {
+            getConversion: getConversion,
+        };
+    }]);
+
